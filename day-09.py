d2e558780ea7c812d8fd496dd456494dae2d86ea
Debesh Barik <chandrabarik397@gmail.com>
2025-04-08 22:27:10 +0530

    Add files via upload

---
+++ day-09.py
@@ -0,0 +1,15 @@
+class Solution:
+    def getMinDiff(self, arr,k):
+        # code here
+      n=len(arr)
+      if n==1:
+          return 0
+      arr.sort()
+      ans=arr[-1]-arr[0]
+      for i in range (n-1):
+          if arr[i+1] < k:
+              continue
+          max_height=max(arr[i]+k,arr[-1]-k)
+          min_height=min(arr[0]+k,arr[i+1]-k)
+          ans=min(ans,max_height-min_height)
+      return ans
